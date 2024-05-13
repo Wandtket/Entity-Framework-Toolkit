d6f04ff738204fbaa3a018b233c3b71bcb0c84c6
Tyler Wandtke <wandtket@gmail.com>
2024-05-13 12:42:33 -0500

    Added color ranges

--- EFToolkit/Extensions/RichEditBoxExtensions.cs
+++ EFToolkit/Extensions/RichEditBoxExtensions.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.UI.Xaml.Controls;
+﻿using Microsoft.UI;
+using Microsoft.UI.Xaml.Controls;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Graphics.Printing3D;
 
 namespace EFToolkit.Extensions
 {
@@ -12,6 +14,10 @@ namespace EFToolkit.Extensions
     {
         public static void SetText(this RichEditBox text, string Value)
         {
+            List<ColorRange> colorRanges = new List<ColorRange>();
+            
+
+
             text.Document.SetText(Microsoft.UI.Text.TextSetOptions.None, Value);
         }
 
@@ -20,5 +26,34 @@ namespace EFToolkit.Extensions
             text.Document.GetText(Microsoft.UI.Text.TextGetOptions.None, out string value);
             return value;
         }
+
+        public static void ColorCode(this RichEditBox editor)
+        {                    
+            var selectedText = editor.TextDocument.Selection.se("string", editor.GetText().Length, Microsoft.UI.Text.FindOptions.None);
+            if (selectedText != null)
+            {
+                var charFormatting = selectedText;
+                charFormatting.ForegroundColor = Colors.Green;
+                selectedText.CharacterFormat = charFormatting;
+            }
+
+
+        }
+
+
+        
+
+        private class ColorRange
+        {
+            public int BeginIndex { get; set; }
+
+            public int EndIndex { get; set; }
+
+            public Colors Color { get; set; }
+
+        }
+
+
+
     }
 }
