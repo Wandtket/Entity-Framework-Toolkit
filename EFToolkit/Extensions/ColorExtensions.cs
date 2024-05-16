c26076a3de7483ef4463181d5e7b6008251b218b
Tyler Wandtke <wandtket@gmail.com>
2024-05-15 22:24:10 -0500

    Color extensions for theme aware colors

---
+++ EFToolkit/Extensions/ColorExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFToolkit.Extensions
+{
+    public static class ColorExtensions
+    {
+        public static Windows.UI.Color IsThemeAware(this Windows.UI.Color color)
+        {
+            return Windows.UI.Color.FromArgb(110, color.R, color.G, color.B);
+        }
+    }
+
+
+}
