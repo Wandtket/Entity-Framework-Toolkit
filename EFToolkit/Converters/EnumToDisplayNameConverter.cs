942b7958068892860bf7377700d6e0864e987d5b
Tyler Wandtke <wandtket@gmail.com>
2024-05-15 22:23:57 -0500

    Added Enum To Display Name Converter

---
+++ EFToolkit/Converters/EnumToDisplayNameConverter.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFToolkit.Converters
+{
+    public class EnumToDisplayNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is Enum enumValue)
+            {
+                var displayAttribute = enumValue.GetType()
+                    .GetMember(enumValue.ToString())
+                    .FirstOrDefault()
+                    ?.GetCustomAttribute<DisplayAttribute>();
+
+                return displayAttribute?.GetName() ?? $"Unknown value: {value}";
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
