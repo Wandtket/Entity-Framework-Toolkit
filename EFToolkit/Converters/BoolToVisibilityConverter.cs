a64a7ebcdfdcd8094dd7046b61ee988fbf1bf5db
Tyler Wandtke <wandtket@gmail.com>
2024-05-23 16:14:36 -0500

    added bool to visibility and removed EnumToDisplayName

---
+++ EFToolkit/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace EFToolkit.Converters
+{
+    internal class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null) { return Visibility.Collapsed; }
+
+            bool v = (bool)value;
+            if (!v)
+            {
+                return Visibility.Collapsed;
+            }
+
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null) { return false; }
+
+            Visibility v = (Visibility)value;
+            if (v == Visibility.Collapsed)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
