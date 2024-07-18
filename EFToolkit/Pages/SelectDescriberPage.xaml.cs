de6c66caf3ff46f7ff9ee63b66448a9997f93329
Tyler Wandtke <wandtket@gmail.com>
2024-07-18 15:59:55 -0500

    Added Teach Tips

--- EFToolkit/Pages/SelectDescriberPage.xaml.cs
+++ EFToolkit/Pages/SelectDescriberPage.xaml.cs
@@ -14,6 +14,7 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using EFToolkit.Extensions;
 using EFToolkit.Controls.Dialogs;
+using System.Threading.Tasks;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -32,17 +33,21 @@ namespace EFToolkit.Pages
 
         public void ToggleTeachTips(bool Toggle)
         {
-            //TeachTip.IsOpen = Toggle;
+            SelectStatementInputTeachTip.IsOpen = Toggle;
+            SelectStatementOutputTeachTip.IsOpen = Toggle;
         }
 
         private async void Input_TextChanged(object sender, RoutedEventArgs e)
         {
             if (Input.GetText() == "") { Output.SetText(""); return; }
 
-            Output.SetText(Toolkit.ConvertSelectToDescriber(Input.GetText()));
+            await Output.SetText(Toolkit.ConvertSelectToDescriber(Input.GetText()));
         }
 
-
-
+        private async void Input_Paste(object sender, TextControlPasteEventArgs e)
+        {
+            await Task.Delay(50);
+            await Input.SetText(Input.GetText());
+        }
     }
 }
